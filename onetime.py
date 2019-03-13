f76537d7a4e5ba7046e6fd4a25a31ada8f2e2d66
chrisole <35532228+chrisole@users.noreply.github.com>
2019-03-13 15:01:48 +0800

    Add files via upload

---
+++ onetime.py
@@ -0,0 +1,99 @@
+import os, argparse, sys, random, math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.misc.pilutil import imshow
+import math
+
+ap = argparse.ArgumentParser()
+
+args = vars(ap.parse_args())
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "config/coco/"))  # To find local version
+import coco
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images/test13.jpg")
+#IMAGE_DIR2 = os.path.join(ROOT_DIR, "images/depth.jpg")
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+            'bus', 'train', 'truck', 'boat', 'traffic light',
+            'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+            'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+            'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+            'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+            'kite', 'baseball bat', 'baseball glove', 'skateboard',
+            'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+            'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+            'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+            'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+            'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+            'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+            'teddy bear', 'hair drier', 'toothbrush']
+
+image = skimage.io.imread(os.path.join(IMAGE_DIR))
+#image2= skimage.io.imread(os.path.join(IMAGE_DIR2),as_grey=True)
+# Run detection
+
+#imshow(image[800:1000,0:200,])
+
+
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+
+j=0
+for i in r['class_ids']:
+    if i == 33:
+        break;
+    else:
+        j+=1
+  
+print(r['rois'][j]) 
+
+x=math.ceil((r['rois'][j][0]+r['rois'][j][2])/2)
+y=math.ceil((r['rois'][j][1]+r['rois'][j][3])/2)
+print(x,y)
+#print(image2[x,y])
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+
+
